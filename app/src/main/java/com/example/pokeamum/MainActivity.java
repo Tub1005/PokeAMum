57072cafe54d821ebd1c84ca05f80ab5f7a253d0
HenrikVP <hpo@tec.dk>
2024-01-30 13:01:10 +0100

    Its (kinda) working

--- app/src/main/java/com/example/pokeamum/MainActivity.java
+++ app/src/main/java/com/example/pokeamum/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,13 +21,16 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TextView text;
     EditText searchField;
+    LinearLayout searchBar;
     ImageButton searchButton;
+    Spinner searchResultSpinner;
     RequestQueue requestQueue;
     List<PokemonCard> cards;
 
@@ -37,39 +43,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         requestQueue = Volley.newRequestQueue(this);
         searchButton.setOnClickListener(this);
         getAllCards();
-
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
     private void initGui() {
         searchField = findViewById(R.id.et_search);
         searchButton = findViewById(R.id.btn_search);
-        text = findViewById(R.id.tv_test);
+        searchResultSpinner = findViewById(R.id.sp_searchresult);
+        searchBar = findViewById(R.id.ll_searchbar);
     }
 
     @Override
     public void onClick(View view) {
-        //TODO call api with searchtext
+        if (cards == null) return;
+
+        List<PokemonCard> searchList = new ArrayList<>();
+        for (PokemonCard pc : cards) {
+            if (pc.name.toLowerCase().contains(searchField.getText().toString().toLowerCase())) {
+                searchList.add(pc);
+            }
+        }
+        createSpinner(searchList);
+    }
+
+    private void createSpinner(List<PokemonCard> pcList) {
+        List<String> pcNames = pcList.stream().map(PokemonCard -> PokemonCard.name + PokemonCard.id)
+                .collect(Collectors.toList());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_dropdown_item, pcNames);
+        searchResultSpinner.setAdapter(adapter);
     }
 
     private void getAllCards() {
         String url = "https://api.tcgdex.net/v2/en/cards";
 
         StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
-
-            cards = new Gson().fromJson(response, new TypeToken<List<PokemonCard>>(){}.getType());
+            cards = new Gson().fromJson(response, new TypeToken<List<PokemonCard>>() {}.getType());
+            Toast.makeText(this, "Cards: " + cards.size(), Toast.LENGTH_LONG).show();
+            searchBar.setVisibility(View.VISIBLE);
             Log.d("Cards", String.valueOf(cards.size()));
-            String str = "";
-            for (PokemonCard pc: cards
-            ) {
-                str += pc.name + "\n";
-
-            }
-            text.setText(str);
-            }, error -> {
-            Toast.makeText(this, error.toString(),Toast.LENGTH_LONG).show();
-
-        });
+        }, error -> Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
+        );
         requestQueue.add(request);
     }
 }
